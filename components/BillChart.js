fc1bdd80ac615a3c65675de9e6f7ce8b42536765
Daniel Hiebert <dan_hiebert@ymail.com>
2021-04-28 23:31:58 +0000

    Fetching bill json and successfully parsing

--- components/BillChart.js
+++ components/BillChart.js
@@ -1,7 +1,9 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { Button, MenuItem } from "@blueprintjs/core";
 import { Select } from "@blueprintjs/select";
+import {  ItemRenderer } from "@blueprintjs/select";
 import {useState, useEffect} from "react"
+import { resolveHref } from 'next/dist/next-server/lib/router/router';
 const testVoteData = [
   {
     name: 'Page A',
@@ -54,25 +56,34 @@ function BillChart(){
       const [billIDList, setbillIDList] = useState({'value':[]});
       const updateVoteGraph = (e)=>{
         e.preventDefault();
-        fetch('/TestData.json').then(res=>res.json())
-        .then(data => {setVoteData(data)})
+        console.log('Getting /data/'+e.target[0].value+'.json')
+        fetch('/data/'+e.target[0].value+'.json')
+        .then(res=>res.json())
+        .then(data => {
+          console.log(data)
+        })
+        
+        
+        .catch((error)=>{
+          console.log(error)
+          console.log(`Error with ${e.target[0].value}`)
+        })
     }
     
-    const getBillIDs = (e) =>{
-      e.preventDefault();
-      fetch('/BillIDs.json').then((res)=>{
-          console.log(res.json())
-      })
-    }
+    
      useEffect(()=>{
       setVoteData(testVoteData)
+      fetch('/BillIDs.json').then(res=>res.json())
+      .then(data => {
+        setbillIDList(data)})
+
      }, [])
      return <>
          <form onSubmit={updateVoteGraph} >
             <input id="BillID"></input>
             <button type='submit' >Search</button>
          </form>
-         
+       
         <LineChart width={600} height={300} data={voteData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
             <Line type="monotone" dataKey="uv" stroke="#8884d8" />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
